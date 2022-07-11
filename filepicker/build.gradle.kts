f5113dc4694b15c9dfdb1a548ae781539d708fcf
mobin yardim <mobinyardim@gmail.com>
2022-07-11 09:43:14 +0430

    artifactory code added

--- filepicker/build.gradle.kts
+++ filepicker/build.gradle.kts
@@ -3,9 +3,105 @@ plugins {
     kotlin("android")
     id("kotlin-parcelize")
     kotlin("kapt")
+    id("com.jfrog.artifactory")
+    id("maven-publish")
 }
 
-version = "2.2.4"
+val libraryGroupId = "ir.inbo.libs"
+val libraryArtifactId = "ComposeFilePicker"
+val libraryVersion = "2.5.0"
+
+
+val sourcesJar = tasks.register("sourcesJar", Jar::class) {
+    from(android.sourceSets["main"].java.getSourceFiles())
+    archiveClassifier.set("sources")
+}
+
+
+val javadoc = tasks.register("javadoc", Javadoc::class) {
+    source = android.sourceSets["main"].java.getSourceFiles()
+    classpath += project.files(android.bootClasspath)
+}
+
+
+val javadocJar = tasks.register("javadocJar", Jar::class) {
+    dependsOn(javadoc)
+    from(javadoc)
+    archiveClassifier.set("javadoc")
+}
+
+
+publishing {
+    publications {
+        afterEvaluate {
+
+            create<MavenPublication>("release") {
+                from(components["release"])
+                artifact(sourcesJar)
+                artifact(javadocJar)
+
+                groupId = libraryGroupId
+                artifactId = libraryArtifactId
+                version = libraryVersion
+
+                // And here are some more properties that go into the pom file.
+                pom {
+                    packaging = "aar"
+                    name.set("Inbo Compose File Picker")
+                    description.set("This module is used for select file")
+                    url.set("https://github.com/inboofficial/ComposeFilePicker")
+                    licenses {
+                        license {
+                            name.set("Mobin Yardim")
+                            url.set("github.com/inboOfficial")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Mobin Yardim")
+                            email.set("mobinyardim@gmail.com")
+                        }
+                    }
+                    scm {
+                        url.set(pom.url.get())
+                        connection.set("scm:git:${url.get()}.git")
+                        developerConnection.set("scm:git:${url.get()}.git")
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://inbo.jfrog.io/artifactory/inbo-public-repo/")
+            credentials {
+                username = properties["artifactory_username"] as String
+                password = properties["artifactory_password"] as String
+            }
+        }
+    }
+}
+
+
+artifactory {
+    setContextUrl("https://inbo.jfrog.io/artifactory")
+    publish {
+        repository {
+            setRepoKey("libs-release-local")
+            setUsername(properties["artifactory_username"] as String)
+            setPassword(properties["artifactory_password"] as String)
+        }
+        defaults {
+            publications("aar")
+            setPublishArtifacts(true)
+            properties.put("qa.level", "basic")
+            properties.put("q.os", "android")
+            properties.put("dev.team", "core")
+            setPublishPom(true)
+        }
+    }
+}
 
 android {
     compileSdk = 32
